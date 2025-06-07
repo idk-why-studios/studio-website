8ed0dc90fcf53e60fd3393d783fee5b58f8c4fe7
Swassyman <swassyman2005@gmail.com>
2025-06-07 16:31:43 +0530

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import '../app.css';
+	export let data;
+	let studioTitle = "idk why studios";
+</script>
+
+<div class="min-h-screen flex flex-col bg-black text-white">
+    <header class="bg-black shadow-md shadow-white-700 border-b">
+        <div class="container mx-auto p-6 flex justify-between items-center">
+            <a href="/">
+                <div class="flex items-center gap-4">
+                    <img class="w-10 h-10" src="/idkwhy.jpg" alt="studio logo" />
+                    <h1 class="font-bold text-xl">{studioTitle}</h1>
+                </div>
+            </a>
+            <nav class="flex flex-col sm:flex-row sm:gap-6 gap-2 text-white font-medium items-center text-sm sm:text-base">
+                <a href="/about" class="hover:text-gray-400 transition-colors">About</a>
+                <a href="#devblog" class="hover:text-gray-400 transition-colors">Dev Blog</a>
+                <a href="#games" class="hover:text-gray-400 transition-colors">Showcase</a>
+            </nav>
+
+        </div>
+    </header>
+
+    <main class="flex-1 container mx-auto p-6 flex items-center justify-center flex-col text-center">
+        <slot />
+    </main>
+
+    <footer class="bg-black text-center py-6 text-sm text-white-500 border-t">
+        © {new Date().getFullYear()} idk why studios. All rights reserved.
+    </footer>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {goto} from "$app/navigation";
+</script>
+
+<section id="home" class="flex flex-col items-center justify-center text-center px-4 py-20">
+    <div>
+        <p class="mt-4 text-lg text-white-700">Welcome!</p>
+        <button
+        on:click={() => goto("/about")}
+        class="mt-6 px-3 py-2 bg-black text-white rounded hover:bg-white hover:text-black border">
+            Read about us
+        </button>
+    </div>
+</section>
