fd8c3ce9cd5d189c53249c43ebe639efa95cc2ba
Swassyman <swassyman2005@gmail.com>
2025-06-19 14:25:49 +0530

    some fixes

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -2,10 +2,8 @@
 	let studioTitle = "idkwhystudios"
 	const cofounders = [
 		{ name: 'Catalyst', img: '/catalyst.jpg', team: 'Lead Programmer', url: "https://github.com/AmalBiju0104"},
-		{ name: 'Modkavartini', img: '/modka.jpg' , team: 'Lead Artist', url: "https://github.com/modkavartini"},
 		{ name: 'RBJ', img: '/rbj.png', team: 'Lead Composer', url: "https://github.com/R-B-J"},
 		{ name: 'Swassy', img: '/swassy.png', team: 'Game Director', url: "https://github.com/Swassyman"},
-		{ name: 'MrValmeeki', img: '/mrvalmeeki.jpg', team: 'Lead QA', url: "https://github.com/MrValmeeki"}
 	];
 </script>
 
